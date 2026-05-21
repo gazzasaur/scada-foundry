034f83bb3d27f52b1584c82dc0a3e4b6f7610937
Gaz <30276587+gazzasaur@users.noreply.github.com>
2026-05-22 09:52:21 +1000

    Adding queuing mechanism to iccp stack

--- scada-foundry-service/src/iccp/mod.rs
+++ scada-foundry-service/src/iccp/mod.rs
@@ -3,14 +3,18 @@ use std::{
 };
 
 use rusty_iccp::RustyIccpClient;
-use rusty_mms_service::{RustyMmsServiceFactory, datapump::MmsServiceDataPump};
+use rusty_mms_service::{RustyMmsServiceFactory, RustyTpktClientConnectionFactory, TpktClientConnectionFactory, datapump::MmsServiceDataPump};
 use tokio::sync::{Mutex, mpsc::{self, UnboundedReceiver, error::TryRecvError}};
 use tokio::sync::{RwLock, mpsc::UnboundedSender};
 
 use crate::config::iccp::{IccpConfiguration, InitiatorIccpAssociation};
 
 enum IccpConfigure {
-    CreateInitiator(InitiatorIccpAssociation),
+    CreateInitiator {
+        listeners: Arc<RwLock<Vec<UnboundedSender<IccpEvent>>>>,
+        association: InitiatorIccpAssociation
+    },
+    Close,
 }
 
 pub enum IccpEvent {
@@ -43,6 +47,12 @@ impl IccpManager {
     }
 }
 
+impl Drop for IccpManager {
+    fn drop(&mut self) {
+        self.configure.send(IccpConfigure::Close);
+    }
+}
+
 enum InitiatorAssociationState {
     New(InitiatorIccpAssociation),
     // Connecting(InitiatorIccpAssociation, UnboundedReceiver<Result<RustyIccpClient, Box<dyn Error>>>),
@@ -62,6 +72,7 @@ impl IccpManagerWorker {
 
         loop {
             match self.configure.try_recv() {
+                Ok(IccpConfigure::Close) => return,
                 Ok(IccpConfigure::CreateInitiator(initiator)) => {
                     initiator_associations.push(InitiatorAssociationState::New(initiator));
                 }
@@ -70,10 +81,10 @@ impl IccpManagerWorker {
                 Err(TryRecvError::Disconnected) => return,
             }
 
-            for initiator_association in &initiator_associations {
+            for initiator_association in initiator_associations.drain(..) {
                 match initiator_association {
                     InitiatorAssociationState::New(initiator_iccp_association) => {
-                        tokio::task::spawn(iccp_initiator_connect(self.data_pump.clone()));
+                        tokio::task::spawn(iccp_initiator_connect(self.data_pump.clone(), initiator_iccp_association));
                     },
                     // InitiatorAssociationState::Connecting(initiator_iccp_association) => todo!(),
                     // InitiatorAssociationState::Connected(initiator_iccp_association, rusty_iccp_client) => todo!(),
@@ -84,7 +95,18 @@ impl IccpManagerWorker {
 }
 
 // TODO Allow connections to use separate or the same data pumps
-async fn iccp_initiator_connect(data_pump: Arc<MmsServiceDataPump>) {
+async fn iccp_initiator_connect(data_pump: Arc<MmsServiceDataPump>, initiator_iccp_association: InitiatorIccpAssociation) {
+    if let Err(e) = try_iccp_initiator_connect(data_pump, initiator_iccp_association).await {
+
+    }
+}
+
+async fn try_iccp_initiator_connect(data_pump: Arc<MmsServiceDataPump>, initiator_iccp_association: InitiatorIccpAssociation) -> Result<(), anyhow::Error> {
+    let host = initiator_iccp_association.remote_control_center.host;
+    let port = initiator_iccp_association.remote_control_center.port;
+    let socket_address = format!("{host}:{port}");
     let factory = RustyMmsServiceFactory::new(data_pump);
-    
+    RustyTpktClientConnectionFactory::new(socket_address.into()?);
+    factory.create_client_connection(tpkt_connection_factory, parameters);
+    Ok(())
 }
